79c89db34db961a937215aa33864a497538201b8
Paolo Rechia <paolorechia@gmail.com>
2017-01-03 10:43:49 -0200

    resizing links

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
-<a href=sandbox/index.php>sandbox</href>
+<a href=sandbox/index.php> <h1> sandbox <h1>
 <br>
-<a href=pongGame/index.php>pong</href>
+<a href=pongGame/index.php> pong 
 
 </html>
