15ffebaf87a1e795fad94226512220d713ce856b
Paolo Rechia <paolorechia@gmail.com>
2017-01-09 21:17:07 -0200

    functions prototypes

--- fireTest/fire.js
+++ fireTest/fire.js
@@ -1,54 +1,58 @@
 var c = document.getElementById("umCanvas");
 var ctx = c.getContext("2d");
-var width = 40;
-var height = 40;
+var width = 200;
+var height = 200;
 var fire = new Array(height);
-for (var i = 0; fire.length; i++){
+
+console.log(fire.length);
+
+for (var i = 0; i < fire.length; i++){
 	fire[i] = new Array(width);
 }
+
 var x = 200;
 var y = 200;
 
 
-/*
+
 function generateFire(){
 	var i = 0;
 		for (var j = 0; j < width; j++){
 			var random = Math.random();
 			if (random > 0.5){
-				fire[i][j]="(255;
+				fire[i][j]=255;
 			}
 			else{
-				fire[i][j];	
+				fire[i][j]=0;	
 			}
 		}
 	for (var i = 1; i < height; i++){
 		for (var j = 0; j < width; j++){
-			
+			fire[i][j]=0;
 		}
 	}
 }
-/*
+
 function drawFire(){
 	for (var i = 0; i < height; i++){
 		for (var j = 0; j < width; j++){
 			ctx.beginPath();
-			ctx.fillStyle=
+			ctx.fillStyle="rgb(" + fire[i][j] + ", 0, 0)";
 			ctx.fillRect(y + j, i + x, 1, 1);
 		}
 	}
 }
-*/
 
+generateFire();
 function mainLoop(){
 
 	ctx.fillStyle="rgb(100, 100, 100)";
 	ctx.fillRect(0,0,c.width,c.height);
+	drawFire();
 
 
-	setTimeout(function(){
-		requestAnimationFrame(mainLoop)
-	}, 1);
+    requestAnimationFrame(mainLoop)
 }
+
 
 mainLoop();
