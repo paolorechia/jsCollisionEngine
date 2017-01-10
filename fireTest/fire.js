a7af49db5f1e46c22a64f5f8f31572f4b5c94906
Paolo Rechia <paolorechia@gmail.com>
2017-01-09 22:47:44 -0200

    best model so far...

--- fireTest/fire.js
+++ fireTest/fire.js
@@ -1,7 +1,7 @@
 var c = document.getElementById("umCanvas");
 var ctx = c.getContext("2d");
-var width = 20;
-var height = 20;
+var width = 12;
+var height = 12;
 var red = new Array(height);
 var green = new Array(height);
 var blue = new Array(height);
@@ -19,9 +19,7 @@ for (var i = 0; i < blue.length; i++){
 var x = 200;
 var y = 200;
 
-
-
-function generateColor(color, intensity = 255, alternate = 6){
+function generateColor(color, intensity = 255, alternate = 6, craziness = 2){
 	var i = 0;
 		for (var j = 0; j < width; j++){
 			var random = Math.random();		
@@ -47,7 +45,7 @@ function generateColor(color, intensity = 255, alternate = 6){
 			}
 		}
 		else{
-			if (i % 2 != 0){
+			if ((i % craziness) != 0){
 			for (var j = 1; j < width - 1; j++){
 				color[i][j] = (color[i-1][j] + color[i-1][j-1] + color[i-1][j+1]) / 3;
 			}
@@ -57,50 +55,36 @@ function generateColor(color, intensity = 255, alternate = 6){
 }
 
 
-function drawFire(){
+function drawFire(red, green){
 	for (var i = 0; i < height; i++){
 		for (var j = 0; j < width; j++){
 			ctx.beginPath();
 			if (red[j][i] > 0){
-				ctx.fillStyle="rgb(" + red[j][i] + ", 0, 0)";
+				ctx.fillStyle="rgb(255, 0, 0)";
+				//ctx.fillStyle="rgb(" + red[j][i] + ", 0, 0)";
 				ctx.fillRect(y + j, i + x, 1, 1);
 			}
 				if (green[j][i] > 0){
 					ctx.fillStyle="rgb(255, 255, 0)";
-				//	ctx.fillStyle="rgb(" + green[j][i] + ", " + green[j][i] + ", " + "0)";
+					//ctx.fillStyle="rgb(" + green[j][i] + ", " + green[j][i] + ", " + "0)";
 					ctx.fillRect(y + j, i + x, 1, 1);
 				}
 		}
 	}
 }
-/*
-function drawColored(){
-	for (var i = 0; i < height; i++){
-		for (var j = 0; j < width; j++){
-			ctx.beginPath();
-			if (red[i][j] > 0){
-				ctx.fillStyle="rgb(" + red[i][j] + ", " + green[i][j] + ", " + blue[i][j] + ")";
-				ctx.fillRect(y + j, i + x, 1, 1);			
-			}
-			else{
-				ctx.fillStyle="rgb(" + red[i][j] + ", " + red[i][j]
-			}
-		}
-	}
-}
-*/
-generateColor(red, 255, 12);
-generateColor(green, 255, 12);
+generateColor(red, 255, 4, 2);
+generateColor(green, 255, 4, 2);
 
+var i = 10;
 function mainLoop(){
 
 	ctx.fillStyle="rgb(0, 0, 0)";
 	ctx.fillRect(0,0,c.width,c.height);
 
-	generateColor(red);
-	generateColor(green, 127);
-	drawFire();
+	generateColor(red, 255, 4, 2);
+	generateColor(green, 255, 4, 2);
 
+	drawFire(red, green);
 
     requestAnimationFrame(mainLoop)
 }
