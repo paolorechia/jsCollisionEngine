6e62bec066576a5f5a713780ba1dcd6bc2980240
Paolo Rechia <paolorechia@gmail.com>
2017-01-11 19:24:20 -0200

    experimating meta tag for mobile

--- asteroids/index.php
+++ asteroids/index.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> <!--320-->
 </head>
 <body>
 <canvas id="umCanvas" width="800" height="600">
