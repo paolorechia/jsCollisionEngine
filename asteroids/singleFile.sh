0cb6e52112837e43e8bb1eb2c24111952e0135fe
Paolo Andreas Stall Rechia <pasr13@inf.ufpr.br>
2017-01-19 14:16:54 -0200

    3 modules so far: shipBase, shipPresets, weapon

--- asteroids/singleFile.sh
+++ asteroids/singleFile.sh
@@ -18,8 +18,9 @@ cat << DOCSTART >$file
 DOCSTART
 
 java -jar ~/bin/closure-compiler-v20161201.jar ../collision.js >> $file
+java -jar ~/bin/closure-compiler-v20161201.jar shipBase.js         >> $file
 java -jar ~/bin/closure-compiler-v20161201.jar weapon.js         >> $file
-java -jar ~/bin/closure-compiler-v20161201.jar ship.js         >> $file
+java -jar ~/bin/closure-compiler-v20161201.jar shipPresets.js         >> $file
 java -jar ~/bin/closure-compiler-v20161201.jar main.js         >> $file
 java -jar ~/bin/closure-compiler-v20161201.jar input.js        >> $file
 cat <<DOCEND >>$file
