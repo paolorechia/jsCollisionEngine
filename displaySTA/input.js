db8cd901da698efec9a462a4b09a28c97c26ab35
Paolo Andreas Stall Rechia <pasr13@inf.ufpr.br>
2017-01-22 10:50:08 -0200

    displaySTA shows debuggingSTA; debugging most likely done

---
+++ displaySTA/input.js
@@ -0,0 +1,43 @@
+var coord = new Point(0, 0);
+
+    function pegaCoordenadas(event){
+        coord.x = event.clientX;
+        coord.y = event.clientY;
+ //       console.log(coord);
+    }
+
+    keyboard = function (event){
+        if (event.key == 'w'){
+//			console.log('w');
+        }
+        if (event.key == 's'){
+        }
+        if (event.key == 'd'){
+            rotatePolygon(objects[0], 1);
+        }
+        if (event.key == 'a'){
+            rotatePolygon(objects[0], -1);
+        }
+        if (event.key == ' '){
+
+        }
+        if (event.key == 'Escape'){
+        }
+    };
+
+    mousePress = function(state){
+ //       console.log(state);
+    };
+
+	
+	c.addEventListener("mousemove", pegaCoordenadas, false);
+    c.addEventListener("mousedown", function(){ mousePress(1); 
+                                                     },
+                                                     false);
+    c.addEventListener("mouseup", function(){ mousePress(0);
+                                                   }, 
+                                                   false);
+    c.addEventListener("click", function(){
+                                                console.log("click!");},
+                                                false);
+    window.addEventListener("keydown", keyboard, false);
--- displaySTA/main.js
+++ displaySTA/main.js
@@ -1,11 +1,11 @@
 var j = 0;
 var maxSize = c.width/10;
 var minSize = c.width/100;
-var maxSpeed = 6;
-var maxSpin = 4;
-var numberRectangles = 4;
-var numberTriangles = 4;
-var numberCircles= 4;
+var maxSpeed = 0;
+var maxSpin = 0;
+var numberRectangles = 1;
+var numberTriangles = 1;
+var numberCircles= 0;
 var objects = [];
 
 for (i = 0; i < numberRectangles; i++){
@@ -16,7 +16,7 @@ for (i = 0; i < numberTriangles; i++){
 	objects.push(new randomTriangle(maxSize, minSize, maxSpeed, maxSpin));
 }
 for (i = 0; i < numberCircles; i++){
-	objects.push(new randomCircle(maxSize, minSize, maxSpeed, maxSpin));
+	objects.push(new randomCircle(maxSize, minSize, 0, maxSpin));
 }
 var axis_length = 20;
 var lastDate = new Date();
@@ -24,11 +24,14 @@ var fps = new Fps();
 var maxFPS = 1000;
 var interval = 1000/maxFPS;
 
+objects[1].moveTo(new Point(400, 300));
+//objects[0].moveTo(new Point(300, 300));
 function mainLoop(){
 	newDate = new Date();
 	elapsedTime = newDate - lastDate;
 	lastDate = new Date();
 	fps.add(elapsedTime);
+    objects[0].moveTo(coord);
 	ctx.fillStyle="#FFFF00";
 	ctx.fillRect(0,0,c.width,c.height);
 
@@ -42,7 +45,11 @@ function mainLoop(){
 		drawAxes(objects[j], axis_length);
 
 	}
-    checkElasticCollisionsNaive(objects);
+    for (var i = 0; i < objects.length - 1; i++){
+        for (var j = i + 1; j < objects.length; j++){
+            debugSTA(objects[i], objects[j]);
+        }
+    }
 	for (k = 0; k < objects.length; k++){
             if (objects[k].sides == 1){
                 drawCircle(objects[k], "#0000FF", "#FF0000", true, "#00F0FF");
