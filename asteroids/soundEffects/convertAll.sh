a6af5923a387d1c40a4781edd2e01ad22c386808
Paolo Andreas Stall Rechia <pasr13@inf.ufpr.br>
2017-01-23 18:10:18 -0200

    fixed bug on convertAll.sh

--- asteroids/soundEffects/convertAll.sh
+++ asteroids/soundEffects/convertAll.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 inputFiles=$(ls | grep .wav)
+inputFiles=$(echo $inputFiles | tr ' ' '@' | tr '\n' '@')
 #echo $inputFiles
-inputFiles=$(echo $inputFiles | tr ' ' '@' )
-numberFiles=$(echo $inputFiles | grep -o @ | wc -l)
+numberFiles=$(echo $inputFiles | grep -o '@' | wc -l)
+#echo $numberFiles
 index=`expr index $inputFiles $ `
 i=0
 while [ $i -ne $numberFiles ]; do
     index=`expr index $inputFiles @ `
     index=`expr $index - 1`
     substring=${inputFiles:0:$index}
+#    echo $substring
     index=`expr $index - 3`
     output=${substring:0:$index}
     output=${output}mp3
     echo $output
-    ffmpeg -i $substring -codec:a libmp3lame -qscale:a 5 $output
+    ffmpeg -i $substring -codec:a libmp3lame -b:a 8k $output
     index=`expr $index + 4`
     inputFiles=${inputFiles:$index}
     i=`expr $i + 1`
