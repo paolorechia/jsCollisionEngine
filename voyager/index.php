9f8ba028ef7c9d67fcc999e8e590a44d2a0ef5a7
Paolo Andreas Stall Rechia <pasr13@inf.ufpr.br>
2017-01-26 22:30:51 -0200

    updating voyager .php

--- voyager/index.php
+++ voyager/index.php
@@ -3,15 +3,22 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> <!--320-->
 </head>
 <body>
-<canvas id="umCanvas" width="800" height="600">
+<canvas id="camera" width="800" height="600">
+<canvas id="umCanvas" width="1600" height="1200" style="display: none"> 
+<canvas id="background" width="1600" height="1200" style="display: none"> 
+<script src="../webaudio/howler.core.min.js"> </script>
+<script src="../webaudio/howler.spatial.min.js"> </script>
+<script src="../sound.js"> </script> 
 <script src="../collision.js"> </script> 
 <script src="../shipBase.js"> </script> 
 <script src="../weapon.js"> </script> 
 <script src="../shipPresets.js"> </script> 
+<script src="../broadCollision.js"> </script> 
+<script src="../updater.js"> </script> 
 <script src="../buttons.js"> </script> 
 <script src="../cookies.js"> </script> 
 <script src="main.js"> </script> 
-<script src="input.js"> </script> 
+<script src="../asteroids/input.js"> </script> 
 </script>
 </body>
 
