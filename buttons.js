2cf9cea5371eb922a0a09585e1279def38c6a2ee
Paolo Andreas Stall Rechia <pasr13@inf.ufpr.br>
2017-01-29 14:18:46 -0200

    second version, still not funuctional

--- buttons.js
+++ buttons.js
@@ -8,39 +8,63 @@ var ButtonScroller = function(x, y, buttons, size){
     this.upButton = new Button(x, y - 30, 
                                buttons[0].width, 
                                buttons[0].height/2);
+    this.upButton.onClick = function(){
+        window.buttonScroller.scrollUp();
+    }
     this.downButton = new Button(x, y + (buttons[0].height * size) + 30,
                                  buttons[0].width, buttons[0].height/2);
+    this.downButton.onClick = function(){
+        window.buttonScroller.scrollDown();
+    }
     this.init = function(buttons){
         this.resetButtons(this.buttons); 
         this.resetButtons(buttons); 
         for (var i = 0; i < this.size; i++){
             this.displaying[i]=this.buttons[i];
         }
+        window.buttons.push(this.upButton);
+        window.buttons.push(this.downButton);
     }
     this.scrollUp = function(){
         if (this.index > 0){
             this.index--;
         }
+        var removedButtons = [];
+        for (var i  = this.index; i < this.size; i++){
+            removedButtons.push(this.buttons[i]);
+        }
+        this.resetButtons(removedButtons);
+        this.setupDisplayingButtons();
     }
     this.scrollDown = function(){
         if (this.index < this.buttons.length){
             this.index++;
         }
+        var removedButtons = [];
+        for (var i  = 0; i < this.index; i++){
+            removedButtons.push(this.buttons[i]);
+        }
+        this.resetButtons(removedButtons);
+        this.setupDisplayingButtons();
     }
     this.setupDisplayingButtons = function(){
-        window.buttons.push(this.upButton);
-        window.buttons.push(this.downButton);
-        for (var i = 0; i < this.size; i++){
+        for (var i = this.index; i < this.size; i++){
             var height = this.displaying[i].height;
             this.displaying[i].x=x;
             this.displaying[i].y=y + (height * i);
-//            window.buttons.push(this.displaying[i]);
+            this.displaying[i].id=this.buttons[i];
+            window.buttons.push(this.displaying[i]);
         }
     }
     this.resetButtons= function(buttons){
         for (var i = 0; i <  buttons.length; i++){
-             buttons[i].x=-200;
-             buttons[i].y=-200;
+            buttons[i].x=-200;
+            buttons[i].y=-200;
+            for (var j = 0; j < window.buttons.length; j++){
+                if (buttons[i].id == window.buttons[j].id){
+                    window.buttons.splice(j, 1);
+                }
+            }
         }
     }
     this.draw = function(){
