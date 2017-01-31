3e856dbc97356a4d5c6bf46995f6cf4ae6fd00f3
Paolo Andreas Stall Rechia <pasr13@inf.ufpr.br>
2017-01-31 11:19:22 -0200

    updating sandbox php

--- sandbox/index.php
+++ sandbox/index.php
@@ -2,6 +2,7 @@
 <body>
 <canvas id="umCanvas" width="800" height="600">
 <script src="../collision.js"> </script>
+<script src="../divisor.js"> </script>
 <script src="input.js"> </script>
 <script src="main.js"> </script>
 </body>
